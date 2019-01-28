6a89847fa550d642afa2e45d30051c07050ab288
timtan93 <timtan93@gmail.com>
2019-01-28 13:08:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching (drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.toUpperCase() === name.toUpperCase()
+  })
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function (driver) {
+      return driver.slice(0, letters.length) === letters
+  })
+}
+
+function matchName (drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver['name'] === name
+  })
+}
